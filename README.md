c56ce7061e18d926e70b1b8a9f602dde204e60d3
Satya Akhil Chowdary K <akhil0@Satyas-MacBook-Pro.local>
2016-09-26 22:47:59 -0400

    Added a simple chromedriver to make the call to Chrome for the user to see the image url

--- README.md
+++ README.md
@@ -4,17 +4,35 @@ Simple Grabber for Instagram pics (extracts pic url)
 Requirements :
 
 1) Java
+
 2) Maven
+
 3) GNUmake
+
 
 
 Steps :
 
 1) CD to the project folder and run 'make jar' command
+
 2) Run the project with 'make run ex_url', where ex_url is the url of the instagram pic you want to grab.
+
+
+Bypass : 
+
+1) Used Selenium driver to open Chrome through chromedriver
+
+
+
+Next Immediate Steps:
+
+1) Driver is open infinitely, to make sure user sees the image. Have to figure a way to close the image and make the driver quit then.
+
+
 
 Thoughts:
 
 1) Opening chrome doesn't have native support, so planning to move to JS
+
 2) Will pack it up as extension for Google chrome to make it more integrated.
 
--- src/main/java/instagrab/Main.java
+++ src/main/java/instagrab/Main.java
@@ -3,6 +3,9 @@ package instagrab;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;import java.net.URL;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
 	public static void main(String[] args) throws Exception {
@@ -10,11 +13,21 @@ public class Main {
 		String curl = args[0];
 		URL url = new URL(curl);
 
+		System.setProperty("webdriver.chrome.driver", "/Users/akhil0/Downloads/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		  
 		try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
 		    for (String line; (line = reader.readLine()) != null;) {
 		        if(line.contains("og:image")) {
 		        	String[] lineelems = line.split(" ");
-		        	System.out.println(lineelems[6].substring(9, lineelems[6].length()-49));
+		        	String lineop = lineelems[6].substring(9, lineelems[6].length()-49);
+
+		            // And now use this to visit Google
+		            driver.get(lineop);
+		            
+		            //driver.quit();
+
 		        }
 		    }
 		}
